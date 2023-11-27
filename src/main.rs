3641c5e6efa60c9b6682715a5cbfa3827ce360b1
weykon <635883092@qq.com>
2023-11-27 18:02:38 +0800

    completed filter out ssh server alter name

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use homedir::get_my_home;
+use regex::Regex;
+use ssh2::Host;
+use ssh2_config::{ParseRule, SshConfig};
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader};
+use std::ops::Deref;
 use std::path::{Path, PathBuf};
 
 fn main() -> io::Result<()> {
+    println!("processing");
     let home = get_my_home().expect("Sorry, can't find your home directory.");
 
     let config_path = home
@@ -16,8 +21,21 @@ fn main() -> io::Result<()> {
         .parse(&mut reader, ParseRule::STRICT)
         .expect("Failed to parse configuration");
 
-    // Query attributes for a certain host
-    let params = config.
+    let re = Regex::new(r"\b\d{1,3}(\.\d{1,3}){3}\b").unwrap();
+
+    let hosts = config.get_hosts();
+
+    let filtered_hosts:  Vec<&ssh2_config::Host> = hosts
+        .iter()
+        .filter(|h| match h.params.host_name {
+            Some(ref host_name) => re.is_match(host_name),
+            None => false,
+        })
+        .collect();
+
+    filtered_hosts
+        .iter()
+        .for_each(|x| println!("{}", x.pattern[0].pattern));
 
     Ok(())
 }
